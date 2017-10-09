7d23a342e533dcf8e5e0b551cf304481e4207b35
Gochev-Ivan <31308329+Gochev-Ivan@users.noreply.github.com>
2017-10-09 12:04:40 +0200

    Add files via upload

--- functions.py
+++ functions.py
@@ -1,166 +1,195 @@
+import pygame
 from constants import *
 import numpy as np
+import pandas as pd
 
 
-def collision(circle1, circle2):
-    if (circle1.x - circle2.x)**2 + (circle1.y - circle2.y)**2 <= (circle1.radius + circle2.radius)**2:
-        return True
-    return False
+def collision(circle_1, circle_2):
+    return (circle_1.x - circle_2.x)**2 + (circle_1.y - circle_2.y)**2 <= (circle_1.radius + circle_2.radius)**2
 
 
-def boundaries(x, y):
-    x1, y1 = x[0], y[0]
-    x2, y2 = x[1], y[1]
-    if x1 < (radius + width):
-        x1 = (radius + width)
-    if y1 < (radius + width):
-        y1 = (radius + width)
-    if x1 > resolution[0] - (radius + width):
-        x1 = resolution[0] - (radius + width)
-    if y1 > resolution[1] - (radius + width):
-        y1 = resolution[1] - (radius + width)
-    if x2 < (radius + width):
-        x2 = (radius + width)
-    if y2 < (radius + width):
-        y2 = (radius + width)
-    if x2 > resolution[0] - (radius + width):
-        x2 = resolution[0] - (radius + width)
-    if y2 > resolution[1] - (radius + width):
-        y2 = resolution[1] - (radius + width)
-    x = [x1, x2]
-    y = [y1, y2]
-    return x, y
+class Circle:
+    def __init__(self, x=0, y=0, radius=0, mass=1, alpha=0):
+        self.x = x
+        self.y = y
+        self.radius = radius
+        self.mass = mass
+        self.alpha = alpha
+        self.v = 0
 
 
-def get_back(circle):
-    circle.x = np.clip(circle.x, radius, playground[0] - radius)
-    circle.y = np.clip(circle.y, radius, playground[1] - radius)
-    return circle
+class Player(Circle):
+    a_fifa = 0.75
+    v_fifa = 0.88
+    shot_power_fifa = 0.95
+    a_max = 1000 * a_fifa
+    v_max = 500 * v_fifa
+    shot_power_max = 500 * shot_power_fifa
+    shot_power = shot_power_max
+    shot_request = False
+
+    def __init__(self, number):
+        df = pd.read_csv('players.csv', index_col='Index')
+        player = df.loc[number]
+        self.mass = int(player['Kilograms'])
+        self.radius = int(player['Radius'])
+        self.a_fifa = int(player['Acceleration'])
+        self.v_fifa = int(player['Speed'])
+        self.shot_power_fifa = int(player['Radius'])
+
+    def move(self, manager_decision):
+        force = np.clip(manager_decision['force'], -0.5 * self.a_max * self.mass, self.a_max * self.mass)
+        self.alpha = manager_decision['alpha']
+        self.shot_power = np.clip(manager_decision['shot_power'], 0, self.shot_power_max)
+        self.shot_request = manager_decision['shot_request']
+        self.v += force / self.mass * dt
+        self.v = np.clip(self.v, 0, self.v_max)
+        self.x += np.cos(self.alpha) * self.v * dt
+        self.y += np.sin(self.alpha) * self.v * dt
+
+    def draw(self, screen, color):
+        pygame.draw.circle(screen, color, [int(self.x), int(self.y)], self.radius)
+        new_x = self.x + self.radius * np.cos(self.alpha)
+        new_y = self.y + self.radius * np.sin(self.alpha)
+        pygame.draw.line(screen, black, [self.x, self.y], [new_x, new_y], cursor_width)
+
+    def data(self):
+        player_data = {'x': self.x, 'y': self.y, 'alpha': self.alpha, 'mass': self.mass, 'radius': self.radius}
+        return player_data
+
+    def snelius(self):
+        if self.y + self.radius >= ground[3] and np.sin(self.alpha) > 0:
+            self.alpha = -self.alpha
+            self.v *= np.abs(np.cos(self.alpha))
+        if self.y - self.radius <= ground[1] and np.sin(self.alpha) < 0:
+            self.alpha = -self.alpha
+            self.v *= np.abs(np.cos(self.alpha))
+        if self.x + self.radius >= ground[2] and np.cos(self.alpha) > 0:
+            self.alpha = np.pi - self.alpha
+            self.v *= np.abs(np.sin(self.alpha))
+        if self.x - self.radius <= ground[0] and np.cos(self.alpha) < 0:
+            self.alpha = -np.pi - self.alpha
+            self.v *= np.abs(np.sin(self.alpha))
+
+    def reset(self, initial_position, alpha):
+        self.x = initial_position[0]
+        self.y = initial_position[1]
+        self.alpha = alpha
+        self.v = 0
+
+    def clip_velocity(self):
+        self.v = np.clip(self.v, 0, self.v_max)
 
 
-def resolve_collision(circle1, circle2, shoot_requsted):
-    collision_angle = np.arctan2(circle2.y - circle1.y, circle2.x - circle1.x)
+class Ball(Circle):
+    v_max = 1500
+    radius = 15
+    mass = 0.5
 
-    new_x_speed_1 = circle1.v * np.cos(circle1.alpha - collision_angle)
-    new_y_speed_1 = circle1.v * np.sin(circle1.alpha - collision_angle)
-    new_x_speed_2 = circle2.v * np.cos(circle2.alpha - collision_angle)
-    new_y_speed_2 = circle2.v * np.sin(circle2.alpha - collision_angle)
+    def move(self):
+        self.x += np.cos(self.alpha) * self.v * dt
+        self.y += np.sin(self.alpha) * self.v * dt
+        self.v *= 0.99
 
-    final_x_speed_1 = ((circle1.mass - circle2.mass) * new_x_speed_1 + (circle2.mass + circle2.mass) * new_x_speed_2) \
-                      / (circle1.mass + circle2.mass)
-    final_x_speed_2 = ((circle1.mass + circle1.mass) * new_x_speed_1 + (circle2.mass - circle1.mass) * new_x_speed_2) \
-                      / (circle1.mass + circle2.mass)
+    def draw(self, screen):
+        pygame.draw.circle(screen, black, [int(self.x), int(self.y)], self.radius)
+        pygame.draw.circle(screen, white, [int(self.x), int(self.y)], self.radius - 2)
+
+    def snelius(self):
+        goal = post_screen_top < self.y < post_screen_bottom
+        if self.y + self.radius >= playground[3] and np.sin(self.alpha) > 0:
+            self.alpha = -self.alpha
+            self.y = playground[3] - self.radius
+        if self.y - self.radius <= playground[1] and np.sin(self.alpha) < 0:
+            self.alpha = -self.alpha
+            self.y = playground[1] + self.radius
+        if self.x + self.radius >= playground[2] and np.cos(self.alpha) > 0 and not goal:
+            self.alpha = np.pi - self.alpha
+            self.x = playground[2] - self.radius
+        if self.x - self.radius <= playground[0] and np.cos(self.alpha) < 0 and not goal:
+            self.alpha = -np.pi - self.alpha
+            self.x = playground[0] + self.radius
+
+    def reset(self):
+        self.x = center[0]
+        self.y = center[1]
+        self.alpha = 0
+        self.v = 0
+
+    def data(self):
+        ball_data = {'x': self.x, 'y': self.y, 'alpha': self.alpha, 'mass': self.mass, 'radius': self.radius}
+        return ball_data
+
+    def clip_velocity(self):
+        self.v = np.clip(self.v, 0, self.v_max)
+
+
+class Post(Circle):
+    def draw(self, screen):
+        pygame.draw.circle(screen, white, [int(self.x), int(self.y)], self.radius)
+
+
+def resolve_collision(circle_1, circle_2):
+    collision_angle = np.arctan2(circle_2.y - circle_1.y, circle_2.x - circle_1.x)
+
+    new_x_speed_1 = circle_1.v * np.cos(circle_1.alpha - collision_angle)
+    new_y_speed_1 = circle_1.v * np.sin(circle_1.alpha - collision_angle)
+    new_x_speed_2 = circle_2.v * np.cos(circle_2.alpha - collision_angle)
+    new_y_speed_2 = circle_2.v * np.sin(circle_2.alpha - collision_angle)
+
+    final_x_speed_1 = ((circle_1.mass - circle_2.mass) * new_x_speed_1
+                       + (circle_2.mass + circle_2.mass) * new_x_speed_2) / (circle_1.mass + circle_2.mass)
+    final_x_speed_2 = ((circle_1.mass + circle_1.mass) * new_x_speed_1
+                       + (circle_2.mass - circle_1.mass) * new_x_speed_2) / (circle_1.mass + circle_2.mass)
     final_y_speed_1 = new_y_speed_1
     final_y_speed_2 = new_y_speed_2
 
     cos_gamma = np.cos(collision_angle)
     sin_gamma = np.sin(collision_angle)
-    circle1.v_x = cos_gamma * final_x_speed_1 - sin_gamma * final_y_speed_1
-    circle1.v_y = sin_gamma * final_x_speed_1 + cos_gamma * final_y_speed_1
-    circle2.v_x = cos_gamma * final_x_speed_2 - sin_gamma * final_y_speed_2
-    circle2.v_y = sin_gamma * final_x_speed_2 + cos_gamma * final_y_speed_2
+    circle_1.v_x = cos_gamma * final_x_speed_1 - sin_gamma * final_y_speed_1
+    circle_1.v_y = sin_gamma * final_x_speed_1 + cos_gamma * final_y_speed_1
+    circle_2.v_x = cos_gamma * final_x_speed_2 - sin_gamma * final_y_speed_2
+    circle_2.v_y = sin_gamma * final_x_speed_2 + cos_gamma * final_y_speed_2
 
-    pos1 = np.array([circle1.x, circle1.y])
-    pos2 = np.array([circle2.x, circle2.y])
-
-    # get the mtd
-    pos_diff = pos1 - pos2
-    d = np.linalg.norm(pos_diff)
+    x_difference = circle_1.x - circle_2.x
+    y_difference = circle_1.y - circle_2.y
+    d = np.linalg.norm([x_difference, y_difference])
 
     # minimum translation distance to push balls apart after intersecting
-    mtd = pos_diff * (((circle1.radius + circle2.radius) - d) / d)
-
-    # resolve intersection --
-    # computing inverse mass quantities
-    im1 = 1 / circle1.mass
-    im2 = 1 / circle2.mass
+    mtd_x = x_difference * (((circle_1.radius + circle_2.radius) - d) / d)
+    mtd_y = y_difference * (((circle_1.radius + circle_2.radius) - d) / d)
+    im1 = 1 / circle_1.mass if circle_1.mass > 0 else 0
+    im2 = 1 / circle_2.mass if circle_2.mass > 0 else 0
 
     # push-pull them apart based off their mass
-    pos1 = pos1 + mtd * (im1 / (im1 + im2))
-    pos2 = pos2 - mtd * (im2 / (im1 + im2))
-    circle1.x, circle1.y = pos1[0], pos1[1]
-    circle2.x, circle2.y = pos2[0], pos2[1]
+    circle_1.x += mtd_x * (im1 / (im1 + im2))
+    circle_1.y += mtd_y * (im1 / (im1 + im2))
+    circle_2.x -= mtd_x * (im2 / (im1 + im2))
+    circle_2.y -= mtd_y * (im2 / (im1 + im2))
 
-    if circle1.type_of_circle == 'player' and circle2.type_of_circle == 'player':
-        circle1.v = 0.5 * np.sqrt(circle1.v_x**2 + circle1.v_y**2)
-        circle2.v = 0.5 * np.sqrt(circle2.v_x**2 + circle2.v_y**2)
-    if circle1.type_of_circle == 'player' and circle2.type_of_circle == 'ball':
-        print("sudar na igrac so topka!")
-        circle1.v = np.sqrt(circle1.v_x**2 + circle1.v_y**2)
-        if shoot_requsted == True:
-            circle2.v += 250
+    if isinstance(circle_1, Player) and isinstance(circle_2, Player):
+        circle_1.v = player_player_restitution * np.sqrt(circle_1.v_x**2 + circle_1.v_y**2)
+        circle_2.v = player_player_restitution * np.sqrt(circle_2.v_x**2 + circle_2.v_y**2)
+    if isinstance(circle_1, Player) and isinstance(circle_2, Ball):
+        circle_1.v = np.sqrt(circle_1.v_x**2 + circle_1.v_y**2)
+        if circle_1.shot_request:
+            circle_2.v = np.sqrt(circle_2.v_x**2 + circle_2.v_y**2)
+            circle_2.v = circle_1.shot_power * circle_1.mass / (circle_1.mass + circle_2.mass) * (1 + ball_restitution)
         else:
-            circle2.v = 0.8 * np.sqrt(circle2.v_x**2 + circle2.v_y**2)
-        # prior = 1 if shoot_requsted else 0.1
-    circle1.alpha = np.arctan2(circle1.v_y, circle1.v_x)
-    circle2.alpha = np.arctan2(circle2.v_y, circle2.v_x)
+            circle_2.v = ball_restitution_under_player_control * np.sqrt(circle_2.v_x**2 + circle_2.v_y**2)
+    if isinstance(circle_1, Player) and isinstance(circle_2, Post):
+        circle_1.v = player_post_restitution * np.sqrt(circle_1.v_x**2 + circle_1.v_y**2)
+        circle_2.v = 0
+    if isinstance(circle_1, Ball) and isinstance(circle_2, Post):
+        circle_1.v = np.sqrt(circle_1.v_x**2 + circle_1.v_y**2)
+        circle_2.v = 0
 
-    snelius(circle1)
-    snelius(circle2)
+    circle_1.alpha = np.arctan2(circle_1.v_y, circle_1.v_x)
+    circle_2.alpha = np.arctan2(circle_2.v_y, circle_2.v_x)
 
-    return circle1, circle2
+    for circle in [circle_1, circle_2]:
+        if isinstance(circle, Player) or isinstance(circle, Ball):
+            circle.clip_velocity()
+            circle.snelius()
 
-
-def snelius(circle):
-    if circle.y + circle.radius >= playground[1] and np.sin(circle.alpha) > 0:
-        circle.alpha = -circle.alpha
-        if circle.type_of_circle == 'player':
-            circle.v *= np.abs(np.cos(circle.alpha))
-    if circle.y - circle.radius <= 0 and np.sin(circle.alpha) < 0:
-        circle.alpha = -circle.alpha
-        if circle.type_of_circle == 'player':
-            circle.v *= np.abs(np.cos(circle.alpha))
-    if circle.x + circle.radius >= playground[0] and np.cos(circle.alpha) > 0:
-        circle.alpha = np.pi - circle.alpha
-        if circle.type_of_circle == 'player':
-            circle.v *= np.abs(np.sin(circle.alpha))
-    if circle.x - circle.radius <= 0 and np.cos(circle.alpha) < 0:
-        circle.alpha = -np.pi - circle.alpha
-        if circle.type_of_circle == 'player':
-            circle.v *= np.abs(np.sin(circle.alpha))
-    return circle
-
-
-def dribble(player, ball):
-    # ball.v += 20*np.cos(player.alpha)
-    print("driblam!")
-    # ball.v += ball.v / (1 + 0.02*ball.v*dt)
-
-    dribble_speed = 1
-    ball.v += 0.1
-    Cl = 1/(2 + dribble_speed/(ball.v*ball.radius))
-    Cd = 0.55 + 1/(22.5 + 4.2*(dribble_speed/(ball.v*ball.radius))**(2.5)**0.4)
-
-    beta = 45
-    A = ball.radius**2 * np.pi
-    Fd = Cd * A * 1.21 * ball.v ** 2 / 2
-    Fl = Cl * A * 1.21 * ball.v ** 2 / 2
-    a_x = (1 / ball.mass) * ((-Fd) * np.cos(beta) - Fl * np.sin(beta)) * dt
-    a_y = (1 / ball.mass) * (Fl * np.cos(beta) - Fd * np.sin(beta)) * dt
-
-
-    ball.v_x += a_x*dt
-    ball.v_y += a_y*dt
-    # ball.v_x -= 1.21 * ball.v * (Cd * ball.v_x + Cl * ball.v_y)*dt
-    # ball.v_y += 1.21 * ball.v * (Cl * ball.v_x - Cd * ball.v_y)*dt
-    ball.v += np.sqrt(ball.v_x**2 + ball.v_y**2)
-    # ball.move()
-    # print("Cl, Cd , ball_v_x, ball_v_y, ball.alpha: ", Cl, Cd, ball.v_x, ball.v_y, ball.alpha)
-    print("Cl: {:3f} Cd: {:3f} ball_v_x: {:3f} ball_v_y: {:3f}: ball_alpha: {:3f}".format(Cl, Cd, ball.v_x, ball.v_y, ball.alpha))
-
-    ##
-    # F = Cl*A*1.21*dribble_speed**2/2
-    # a = F/ball.mass
-    # ball.v += a*dt
-
-
-
-
-    ##
-    # rotation_speed = 5
-    # v_x = ball.v*np.cos(ball.alpha) + rotation_speed*np.cos(player.alpha)
-    # v_y = ball.v*np.sin(ball.alpha) + rotation_speed*np.sin(player.alpha)
-    # # ball.v_x = ball.v*np.cos(player.alpha)
-    # # ball.v_y = ball.v*np.sin(player.alpha)
-    # ball.v = np.sqrt(v_x**2 + v_y**2)
-    # ball.alpha = np.arctan2(v_y, v_x)
+    return circle_1, circle_2
--- main.py
+++ main.py
@@ -1,91 +1,72 @@
-import pygame
-from constants import *
 from functions import *
 import numpy as np
+import football_manager as football_manager_1
+import football_manager as football_manager_2
+import time
 
 
-class Circle:
-    mass = 1
-    x, y = 50, 50
-    v_x, v_y = 0, 0
-    v = 0
-    alpha = 0
-    radius = 20
-    type_of_circle = ''
+def render(screen, team_1, team_2, ball, posts, team_1_score, team_2_score, time_to_play, start, countdown=False):
+    # screen.blit(background, (0, 0))
+    pygame.draw.rect(screen, white, resolution_rect)
+    pygame.draw.rect(screen, grass, ground_rect)
+    pygame.draw.rect(screen, black, resolution_rect, 2)
+    pygame.draw.rect(screen, black, ground_rect, 2)
+    pygame.draw.rect(screen, white, playground_rect, 2)
+    pygame.draw.rect(screen, white, half_playground_rect, 2)
+    pygame.draw.circle(screen, white, center, 100, 2)
+    pygame.draw.circle(screen, white, center, 5)
 
-    def init(self, x, y, mass):
-        self.x = x
-        self.y = y
-        self.mass = mass
+    for player in team_1:
+        player.draw(screen, team_1_color)
+    for player in team_2:
+        player.draw(screen, team_2_color)
+    ball.draw(screen)
+    for post in posts:
+        post.draw(screen)
 
-    def move(self):
-        self.x += np.cos(self.alpha) * self.v * dt
-        self.y += np.sin(self.alpha) * self.v * dt
-        if self.type_of_circle == 'ball':
-            self.v *= 0.99
+    if countdown:
+        short_pause_countdown_time = 3
+        myfont = pygame.font.SysFont("monospace", 750)
+        short_pause_countdown = "{}".format(short_pause_countdown_time - int(time.time() - start))
+        label = myfont.render(short_pause_countdown, 1, (0, 0, 0))
+        screen.blit(label, (460, 85))
+    else:
+        myfont = pygame.font.SysFont("monospace", 150)
+        message = "{} сек.".format(time_to_play - int(time.time() - start))
+        label = myfont.render(message, 1, (0, 0, 0))
+        screen.blit(label, (700, 0))
 
-    def draw(self):
-        pygame.draw.circle(game, blue, [int(self.x), int(self.y)], self.radius)
-        new_x = self.x + self.radius * np.cos(self.alpha)
-        new_y = self.y + self.radius * np.sin(self.alpha)
-        pygame.draw.line(game, black, [self.x, self.y], [new_x, new_y], cursor_width)
+    myfont = pygame.font.SysFont("monospace", 150)
+    message = "{}:{}".format(team_2_score, team_1_score)
+    label = myfont.render(message, 1, (0, 0, 0))
+    screen.blit(label, (300, 0))
 
-
-class Player(Circle):
-    q = 1
-
-
-class Ball(Circle):
-    q = 1
-
-
-def render():
     pygame.display.flip()
-    clock.tick(fps)
+    pygame.time.Clock().tick(fps)
 
 
-if __name__ == "__main__":
-    pygame.init()
-    myfont = pygame.font.SysFont("monospace", 15)
-    clock = pygame.time.Clock()
-    game = pygame.display.set_mode(resolution)
-    pygame.display.set_caption(gameName)
-    background = pygame.image.load(background_picture).convert()
-    game.blit(background, (0, 0))
-    pygame.display.flip()
-    # define players/teams:
-    gameExit = False
-    x1, y1 = 50, 50
-    x2, y2 = 150, 150
-    dx1, dy1 = 0, 0
-    dx2, dy2 = 0, 0
-    ball_x, ball_y = 100, 100
-    v = 0
-    v_x1, v_y1 = 0, 0
-    velocity_step = 10
+def play(screen, team_1, team_2, ball, posts, time_to_play, team_1_score, team_2_score, half):
+    start = time.time()
+    while time.time() - start < 3:
+        render(screen, team_1, team_2, ball, posts, team_1_score, team_2_score, time_to_play, start, True)
+
+    start = time.time()
+    velocity_step = 100000
     angle_step = 0.1
     angle_change = 0
     velocity_change = 0
-    player_1 = Circle()
-    player_2 = Circle()
-    player_3 = Circle()
-    player_1.x, player_1.y, player_1.mass = 50, 50, 100
-    player_2.x, player_2.y, player_1.mass = 150, 150, 80
-    player_3.x, player_3.y, player_1.mass = 200, 200, 72
-    player_1.type_of_circle = 'player'
-    player_2.type_of_circle = 'player'
-    player_3.type_of_circle = 'player'
-    ball = Circle()
-    ball.radius = 10
-    ball.x, ball.y = 100, 200
-    ball.type_of_circle = 'ball'
-    ball.mass = 0.5
-    shoot_requst = False
 
-    while not gameExit:
+    circles = [team_1[0], team_1[1], team_1[2], team_2[0], team_2[1], team_2[2], ball, posts[0], posts[1], posts[2],
+               posts[3]]
+    goal = False
+    game_exit = False
+    while not game_exit:
+        if time.time() - start >= time_to_play:
+            return False, 0, team_1_score, team_2_score
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                gameExit = True
+                game_exit = True
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_LEFT:
                     angle_change = -angle_step
@@ -93,52 +74,111 @@ if __name__ == "__main__":
                     angle_change = angle_step
                 if event.key == pygame.K_UP:
                     velocity_change = velocity_step
-                    player_1.v += velocity_change
                 if event.key == pygame.K_DOWN:
                     velocity_change = -velocity_step
-                    player_1.v += velocity_change
-                if event.key == pygame.K_s:
-                    # print("sut na topka aktiviran!")
-                    print("dribling aktiviran!")
-                    shoot_requst = False
-                    dribble(player_1, ball)
-                if event.key == pygame.K_d:
-                    print("sut na topka deaktiviran!")
-                    shoot_requst = False
                 if event.key == pygame.K_ESCAPE:
-                    gameExit = True
+                    game_exit = True
             if event.type == pygame.KEYUP:
                 if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
                     angle_change = 0
                 if event.key == pygame.K_UP or event.key == pygame.K_DOWN:
                     angle_change = 0
-                if event.key == pygame.K_a or event.key == pygame.K_d:
+                if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
                     velocity_change = 0
-                if event.key == pygame.K_w or event.key == pygame.K_s:
+                if event.key == pygame.K_UP or event.key == pygame.K_DOWN:
                     velocity_change = 0
 
-        player_1.alpha += angle_change
-        # player_1.v += velocity_change
-        if player_1.v < 0:
-            player_1.v = 0
-        for player in [player_1, ball]:
-            player.move()
+        if not goal:
+            manager_1_decision = football_manager_1.decision(
+                our_team=[team_1[0].data(), team_1[1].data(), team_1[2].data()],
+                their_team=[team_2[0].data, team_2[1].data, team_2[2].data],
+                ball=ball.data(),
+                your_side='left' if half == 1 else 'right',
+                half=half,
+                time_left=time_to_play - int(time.time() - start))
+            manager_2_decision = football_manager_2.decision(
+                our_team=[team_2[0].data(), team_2[1].data(), team_2[2].data()],
+                their_team=[team_1[0].data(), team_1[1].data(), team_1[2].data()],
+                ball=ball.data(),
+                your_side='right' if half == 1 else 'left',
+                half=half,
+                time_left=time_to_play - int(time.time() - start))
 
-        circles = [player_1, ball]
-        for i in range(len(circles)):
-            circles[i] = snelius(circles[i])
-            for j in range(i+1, len(circles)):
-                if collision(circles[i], circles[j]):
-                    circles[i], circles[j] = resolve_collision(circles[i], circles[j], shoot_requsted=shoot_requst)
+        manager_decision = [manager_1_decision[0], manager_1_decision[1], manager_1_decision[2],
+                            manager_2_decision[0], manager_2_decision[1], manager_2_decision[2]]
 
-        game.blit(background, (0, 0))
-        player_1.draw()
-        for player in [player_1]:
-            player.draw()
-        pygame.draw.circle(game, green, [int(ball.x), int(ball.y)], ball.radius)
-        pygame.draw.rect(game, black, [0, 0, playground[0], playground[1]], 2)
-        message = "player_1: alpha: {:3f} v: {:3f} ball_v: {:3f}".format(player_1.alpha * 180 / np.pi, player_1.v, ball.v)
-        label = myfont.render(message, 1, (0, 0, 0))
-        game.blit(label, (20, 260))
-        render()
+        manager_decision[0]['alpha'] += angle_change
+        manager_decision[0]['force'] += velocity_change
+
+        for i, player in enumerate(circles[:-4]):
+            if isinstance(player, Player):
+                player.move(manager_decision[i])
+            else:
+                player.move()
+
+        if not goal:
+            goal_team_1 = post_screen_top < ball.y < post_screen_bottom and ball.x < post_screen_left
+            goal_team_2 = post_screen_top < ball.y < post_screen_bottom and ball.x > post_screen_right
+            if goal_team_1:
+                team_1_score += 1
+            if goal_team_2:
+                team_2_score += 1
+            goal = goal_team_1 or goal_team_2
+        else:
+            return True, time_to_play - int(time.time() - start), team_1_score, team_2_score
+
+        if not goal:
+            for i in range(len(circles[:-4])):
+                circles[i].snelius()
+                for j in range(i + 1, len(circles)):
+                    if collision(circles[i], circles[j]):
+                        circles[i], circles[j] = resolve_collision(circles[i], circles[j])
+
+        render(screen, team_1, team_2, ball, posts, team_1_score, team_2_score, time_to_play, start)
+
+
+def game(team_1, team_2, ball, posts):
+    pygame.init()
+    # screen = pygame.display.set_mode(resolution, pygame.FULLSCREEN)
+    screen = pygame.display.set_mode(resolution)
+    pygame.display.set_caption(game_name)
+    # background = pygame.image.load(background_picture).convert()
+    # screen.blit(background, (0, 0))
+    # pygame.display.flip()
+    team_1_score, team_2_score = 0, 0
+
+    time_to_play = half_time_duration
+    while time_to_play:
+        for i, player in enumerate(team_1):
+            player.reset(initial_positions_team_left[i], 0)
+        for i, player in enumerate(team_2):
+                player.reset(initial_positions_team_right[i], np.pi)
+        ball.reset()
+        goal, time_to_play, team_1_score, team_2_score = \
+            play(screen, team_1, team_2, ball, posts, time_to_play, team_1_score, team_2_score, 1)
+
+    time_to_play = half_time_duration
+    while time_to_play:
+        for i, player in enumerate(team_2):
+            player.reset(initial_positions_team_left[i], 0)
+        for i, player in enumerate(team_1):
+                player.reset(initial_positions_team_right[i], np.pi)
+        ball.reset()
+        goal, time_to_play, team_2_score, team_1_score = \
+            play(screen, team_1, team_2, ball, posts, time_to_play, team_2_score, team_1_score, 2)
+
     pygame.quit()
+
+
+def main():
+    team_1_players = [Player(500), Player(1), Player(2)]
+    team_2_players = [Player(3), Player(4), Player(5)]
+    ball = Ball(420, 250, 15, 0.5)
+    posts = [Post(post_screen_left, post_screen_top, post_radius, 0),
+             Post(post_screen_left, post_screen_bottom, post_radius, 0),
+             Post(post_screen_right, post_screen_top, post_radius, 0),
+             Post(post_screen_right, post_screen_bottom, post_radius, 0)]
+    game(team_1_players, team_2_players, ball, posts)
+
+if __name__ == "__main__":
+    main()
